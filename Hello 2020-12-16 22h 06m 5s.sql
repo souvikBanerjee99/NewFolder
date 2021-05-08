ef4eb13443cfa55a015e294a24cfe917a30caeb7
Souvik <banerjees480@gmail.com>
2021-05-09 02:12:13 +0530

    new changes to be pushed

---
+++ Hello 2020-12-16 22h 06m 5s.sql
@@ -0,0 +1,2 @@
+INSERT INTO regex_builder_version_control_obj_info (`obj_id`,`name`,`create_time`,`path`,`condition1value1`,`condition2value2`) VALUES('2', '1', NOW(), NULL, '67.0', NULL);
+INSERT INTO regex_builder_version_control_obj_info (`obj_id`,`name`,`create_time`,`path`,`condition1value1`,`condition2value2`) VALUES('3', '4', NOW(), NULL, NULL, 'hgvjnbj');
